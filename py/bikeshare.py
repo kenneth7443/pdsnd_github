77d225ea66dfb32c16e279d9331f30205cbeb955
Kenneth Ratshibvumo <dk.ratshibvumo@gmail.com>
2024-02-20 02:08:16 +0200

    Removed extra lines on load_data() function

--- py/bikeshare.py
+++ py/bikeshare.py
@@ -73,9 +73,8 @@ def load_data(city, month, day):
     if month != 'all':
         months = ['january','february','march','april','may','june']
         month = months.index(month) + 1
-        
         df = df[df['month'] == month]
-           
+        
     if day != 'all':
         df = df[df['day_of_week'] == day.title()]
         
